bea06f5a655f0d114f2b7bd2ed5d4ccf9bc52536
Abhinav Srivastava <atg271@gmail.com>
2022-12-15 22:20:34 -0500

    wip

---
+++ src/main/java/org/Launcher.java
@@ -0,0 +1,23 @@
+package org;
+
+import org.userInterface.MainMenu;
+import org.tests.testConnection;
+
+import java.sql.SQLException;
+
+public class Launcher {
+    MainMenu menu = new MainMenu();
+    testConnection test = new testConnection();
+
+    public static void main(String[] args) throws SQLException {
+        Launcher launcher = new Launcher();
+        boolean testPass = launcher.test.testAll("jdbc:sqlite:src/bank.db");
+        System.out.println("Starting application");
+        System.out.println("Database test: " + testPass);
+        if (testPass) {
+            MainMenu.main(args);
+        } else {
+            System.out.println("Database test failed, please check database");
+        }
+    }
+}
